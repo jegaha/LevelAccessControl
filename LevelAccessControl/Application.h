cf567286ceafe603ef884275481e462d40d2d57d
Jens Gabrikowski <jens@gabrikowski.de>
2022-04-29 14:26:34 +0200

    Implement the behaviour to trigger relay with correct cardId

--- LevelAccessControl/Application.h
+++ LevelAccessControl/Application.h
@@ -4,11 +4,18 @@
 #include "Relay.h"
 #include "CardReader.h"
 
+#ifndef APP_RELAY_DURATION_MS
+#define APP_RELAY_DURATION_MS 1000
+#endif
+
 class Application
 {
   private:
     Relay relay;
     CardReader cardReader;
+
+    unsigned long relayOnUntil = 0;
+    bool relayTriggered = false;
 
   public:
     Application(Relay relay, CardReader cardReader)
@@ -18,10 +25,48 @@ class Application
     }
 
     void run() {
+      long cardId = cardReader.getCardId();
+      handleCardId(cardId);
+      handleRelais();
+    }
+
+  private:
+    void handleCardId(long cardId) {
+      if (!relayTriggered && cardId) {
+        Serial.print("card ID: ");
+        Serial.print(cardId);
+        Serial.print(" ");
+
+        if(isValidCard(cardId)) {
+          Serial.println("access granted.");
+          triggerRelay();
+        } else {
+          Serial.println("access denied.");
+          delay(10);
+        }
+      }
+    }
+
+    void handleRelais() {
+      if (relayTriggered) {
+        if (relayOnUntil && (millis() > relayOnUntil)) {
+          relay.off();
+          relayTriggered = false;
+        }
+      }
+    }
+
+    void triggerRelay() {
+      relayOnUntil = millis() + APP_RELAY_DURATION_MS;
+      if (relayOnUntil == 0) {
+        relayOnUntil = 1;
+      }
+      relayTriggered = true;
       relay.on();
-      delay(500);
-      relay.off();
-      delay(500);
+    }
+
+    bool isValidCard(long id) {
+      return id == APP_ALLOWED_CARD_ID;
     }
 };
 
