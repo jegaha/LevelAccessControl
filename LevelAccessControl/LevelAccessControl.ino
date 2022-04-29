7ecb15025556810c4805926023a0cf3e45ca6832
Jens Gabrikowski <jens@gabrikowski.de>
2022-04-29 08:12:40 +0200

    Cut out Relay code into functions to easily disable them during RFID POC

--- LevelAccessControl/LevelAccessControl.ino
+++ LevelAccessControl/LevelAccessControl.ino
@@ -1,10 +1,17 @@
 #define RELAY_TOGGLE_PIN 'D1' // pin 'D1' or GPIO 5
 
 void setup() {
-  pinMode(RELAY_TOGGLE_PIN, OUTPUT); // Set pin as output
+  setupRelay();
 }
 
+void setupRelay() {
+  pinMode(RELAY_TOGGLE_PIN, OUTPUT); // Set pin as output
+}
 void loop() {
+  loopRelay();
+}
+
+void loopRelay() {
   digitalWrite(RELAY_TOGGLE_PIN, HIGH); // Turn pin on
   delay(1000);
   digitalWrite(RELAY_TOGGLE_PIN, LOW); // Turn pin off
