8fdfe717b0032623c9678f2426743a4feca5a3de
Jens Gabrikowski <jens@gabrikowski.de>
2022-05-01 14:29:01 +0200

    Implement a StaServer class to handle the STA mode
    Autmatically shut down StaServer after 5mins
    Move StaServer cofiguration to settings.h
    Add simple WebServer to StaServer

--- LevelAccessControl/LevelAccessControl.ino
+++ LevelAccessControl/LevelAccessControl.ino
@@ -1,20 +1,24 @@
 #include "settings.h"
 
+#include "StaServer.h"
 #include "Ota.h"
 #include "Relay.h"
 #include "CardReader.h"
 #include "CardRepository.h"
 #include "Application.h"
 
+StaServer staServer = StaServer();
 Ota ota = Ota();
 Relay relay = Relay(RELAY_TOGGLE_PIN);
 CardReader cardReader = CardReader(SS_PIN, RST_PIN);
 CardRepository cardRepository = CardRepository();
 
-Application application = Application(relay, cardReader, cardRepository, ota);
+Application application = Application(relay, cardReader, cardRepository, ota, staServer);
 
 void setup() {
   Serial.begin(9600);
+
+  staServer.enable();
 
   ota.begin();
   ota.enable();
@@ -24,6 +28,8 @@ void setup() {
 }
 
 void loop() {
+  staServer.loop();
   ota.loop();
+
   application.run();
 }
